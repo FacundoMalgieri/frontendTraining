388566ca0750845a28c2c1caab2d08ee4f8c018a
Facundo Ariel Malgieri <facundo.malgieri@gmail.com>
2017-08-29 15:51:48 -0300

    Conection established. To do: migrate URLS to environment constants. Receive objects and display them in results section.

--- spotify-client/src/app/services/web.service.ts
+++ spotify-client/src/app/services/web.service.ts
@@ -1,20 +1,48 @@
 import { Http, Response, Headers, RequestOptions } from '@angular/http';
-import { Injectable } from '@angular/core';
+import { Injectable, OnInit } from '@angular/core';
 import { Observable } from 'rxjs/Rx';
 import { ErrorObservable } from "rxjs/observable/ErrorObservable";
 import { environment } from "environments/environment";
+import { ActivatedRoute, Router } from "@angular/router";
 
 @Injectable()
 export class WebService {
-    constructor(private http: Http) { }
+    constructor(
+        private http: Http,
+        private router: Router,
+        private route: ActivatedRoute) { }
 
+    scope = 'user-follow-modify user-follow-read user-library-read user-library-modify user-read-private user-read-birthdate user-read-email user-top-read';
+    clientId: string = 'b6eef19106214c13bd0c2811ea88f407';
+    clientSecret: string = '0166b8230f6b4ed9af333454da5b0c9d'; //not necessary
+    redirectUri: string = 'http://localhost:4200/explorar'
+    response_type = 'token'
+    token: string
+    url = 'https://accounts.spotify.com/authorize?' +
+    'response_type=' + this.response_type +
+    '&client_id=' + this.clientId +
+    '&scope=' + this.scope +
+    '&redirect_uri=' + this.redirectUri;
+
+    generateToken(): void {
+        window.location.href = this.url
+        let tokenRoute = this.route.fragment.map(fragment => fragment);
+        tokenRoute.subscribe(fragment => {
+            let fragment1 = !!fragment ? fragment.match(/^(.*?)&/) : '';
+            if (!!fragment1) {
+                this.token = fragment1[1].replace('access_token=', '');
+                localStorage.setItem('token', this.token)
+            }
+        })
+    }
+    
     /**
      * This method is used to get the current user's from 
      * the local storage JSON value parsed as an object.
      * @return {Object} unnamed - the user object with your data.
      */
     private getCurrentUser(): Object {
-        return JSON.parse(localStorage.getItem('token'));
+        return localStorage.getItem('token');
     }
 
     /**
@@ -24,28 +52,21 @@ export class WebService {
      */
     private createAuthorizationHeader(body?: any) {
         let headers: Headers = new Headers({ 'Content-Type': 'application/json' });
-        let currentUser: any = this.getCurrentUser();
-        
-        if (currentUser && currentUser.token)
-            headers.append('Authorization', 'Bearer ' + currentUser.token)
+        let token: any = this.getCurrentUser();
 
-        if (body)
+        if (token)
+            headers.append('Authorization', 'Bearer ' + token)
+        if (body) {
+        console.log(headers)
             return new RequestOptions({
                 headers: headers,
                 body: body
             });
+        }
 
         return new RequestOptions({ headers: headers });
     }
 
-    /**
-     * This method throws the error status text.
-     * @param {Response} error - the response containing the error.
-     * @return {Observable<any>}
-     */
-    private handleError(error: Response): ErrorObservable {
-        return Observable.throw(error);
-    }
 
     /**
      * This method performs a GET request to the API.
@@ -90,5 +111,14 @@ export class WebService {
         return this.http.delete(environment.baseUrl + url, this.createAuthorizationHeader(body))
             .map((response: Response) => response.json())
             .catch(this.handleError);
+    }
+
+    /**
+     * This method throws the error status text.
+     * @param {Response} error - the response containing the error.
+     * @return {Observable<any>}
+     */
+    private handleError(error: Response): ErrorObservable {
+        return Observable.throw(error);
     }
 }
